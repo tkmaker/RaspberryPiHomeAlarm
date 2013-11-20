c8d58a21fd1d8ba4772c20e143749f9f34a26321
TKMaker <trushk@gmail.com>
2013-11-20 00:26:58 -0800

    modified alarm.py for case where disarm after arm

--- alarm.py
+++ alarm.py
@@ -4,11 +4,30 @@ import sys,os
 import subprocess
 import time 
 
+import RPi.GPIO as GPIO
+
 # Retrieve GPIO lib
-GPIO = webiopi.GPIO
+#GPIO = webiopi.GPIO
 
 os.system("sudo echo -n 0 > codematch.txt")
 proc_start = 0
+
+led = 11 #gpio 17
+
+# to use Raspberry Pi board pin numbers
+GPIO.setmode(GPIO.BOARD)
+
+# set up GPIO output channel for pin 11 GPIO 17
+GPIO.setup(led, GPIO.OUT)
+
+
+def blink(pin):
+        GPIO.output(pin,GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(pin,GPIO.LOW)
+        time.sleep(1)
+        return
+
 
 while True:
            with open("/home/pi/trush_workdir/scripts/homealarm/armed.txt", "r") as fo:
@@ -16,7 +35,7 @@ while True:
               armed_status = fo.read(1)
            fo.closed
            print "Armed status: " + str(armed_status)
-         
+           
            if (armed_status == "1"):
               if (proc_start == 0):
                 p = subprocess.Popen(["sudo","/home/pi/trush_workdir/rf_xmit/433Utils/RPi_utils/RFSniffer"], stdout = subprocess.PIPE,stderr = subprocess.PIPE)
@@ -29,11 +48,19 @@ while True:
               fo1.closed 
               if (codematch_status == "1"):
                 print "RF code match recvd. Sound the alarm!!!"
-#todays_date = datetime.datetime.today()
-#try:
-#  api.update_status_with_media(status = ("Intruder alert: " + todays_date.strftime('%m-%d-%y-%H%M')), media= image_path)
-#except tweetpony.APIError as err:
-#  print "Oops, something went wrong! Twitter returned error #%i and said: %s" % (err.code, err.description)
-
-
+                subprocess.call(["bash","intruder_mail.sh"]) 
+                for i in range(0,5):
+                   blink(led)
+                                
+                #todays_date = datetime.datetime.today()
+                #try:
+                #  api.update_status_with_media(status = ("Intruder alert: " + todays_date.strftime('%m-%d-%y-%H%M')), media= image_path)
+                #except tweetpony.APIError as err:
+                #  print "Oops, something went wrong! Twitter returned error #%i and said: %s" % (err.code, err.description)
+           elif (armed_status == "0"): 
+               os.system("sudo echo -n 0 > /home/pi/trush_workdir/scripts/homealarm/codematch.txt")
+               
 p.kill()
+GPIO.cleanup()
+
+
